f2872edb36dcd391af335d68a01a48b10d4b771e
Tresehshs <162690468+Tresehshs@users.noreply.github.com>
2025-02-10 15:22:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text, end=None)
-    console.print("[bold green]: IF YOU WANT UNLIMITED ACCESS BUY ON @trese_xeno IN TELEGRAM.")
+    console.print("[bold green]: IF YOU WANT UNLIMITED ACCESS BUY ON "trese_xeno" IN TELEGRAM.)
     console.print("[bold green] [/bold green]: SHARE ACCESS IS NOT ALLOWED IF YOU SHARE ACCES YOU GOT BLOCK.")
     console.print("[bold red]==================================================[/bold red]")
     console.print("[bold yellow]! Note[/bold yellow]: BEFORE USING THIS MAKE SURE THE ACCOUNT WAS ALREADY SIGNOUT IN GAME.", end="\n\n")
@@ -168,7 +168,7 @@ if __name__ == "__main__":
             console.print("[bold][cyan](0) :[/cyan] [red]Exit[/red]", end="\n\n")
             service = IntPrompt.ask(f"[bold][?] Select a Service [red][1-{choices[-1]} or 0][/red][/bold]", choices=choices, show_choices=False)
             if service == 0: # Exit
-                console.print(f"[bold yellow][!] Thank You for using our tool, please join our telegram channel.")
+                console.print(f"[bold yellow][!] Thank You for using our tool, please join our telegram channel[/bold yellow]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue].")
             elif service == 1: # Increase Money
                 console.print("[bold cyan][!] Insert how much money do you want.[/bold cyan]")
                 amount = IntPrompt.ask("[bold][?] Amount[/bold]")
