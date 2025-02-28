e226a47dd869f96d9e815290fac42c0ea0285ea8
Tresehshs <162690468+Tresehshs@users.noreply.github.com>
2025-02-28 18:15:17 +0800

    Update tresemain.py

--- tresemain.py
+++ tresemain.py
@@ -181,7 +181,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from Tresecarpark import CarParkTool
+from tresecarpark import CarParkTool
 
 __CHANNEL_USERNAME__ = "XENOCPM"
 __GROUP_USERNAME__   = "trese_xeno"
